f35d7f91af955e0ba6b00417bd519efa17538ceb
sachin-beniwal <sachin@leewayhertz.com>
2022-05-28 17:13:16 +0530

    Updates

---
+++ precache-manifest.edd0bace9fa4aeb5dbf88ff8025157c9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e590b5953698b985802cd0507c0763",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "d1ec1a5384caaffb859b",
+    "url": "/deploy/static/css/2.615825d4.chunk.css"
+  },
+  {
+    "revision": "0b48419320ba5bef9258",
+    "url": "/deploy/static/css/main.2842e32d.chunk.css"
+  },
+  {
+    "revision": "d1ec1a5384caaffb859b",
+    "url": "/deploy/static/js/2.15011def.chunk.js"
+  },
+  {
+    "revision": "5ed1a3e7bf3adfca527dc610123d6b72",
+    "url": "/deploy/static/js/2.15011def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b48419320ba5bef9258",
+    "url": "/deploy/static/js/main.3cb0c1f2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/deploy/static/js/main.3cb0c1f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dcc96c73ee26a66609",
+    "url": "/deploy/static/js/runtime-main.23e8d9f7.js"
+  },
+  {
+    "revision": "2b53d2d1026d0614b63877cdc8753ca9",
+    "url": "/deploy/static/media/Banner.2b53d2d1.webp"
+  },
+  {
+    "revision": "f79b9acf81e4ece9a3acbc5c153516cb",
+    "url": "/deploy/static/media/Frame.f79b9acf.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/deploy/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/deploy/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/deploy/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/deploy/static/media/slick.f97e3bbf.svg"
+  }
+]);
